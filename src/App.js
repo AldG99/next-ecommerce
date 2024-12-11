f9a43a0d8fd8746195062fbc2ef93d0737db7cbb
AldG99 <a.garciaglz17@gmail.com>
2024-12-10 18:29:36 -0600

    Next.js App Router Creación de páginas y diseño

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import ListPage from './list/page';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/list" element={<ListPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
