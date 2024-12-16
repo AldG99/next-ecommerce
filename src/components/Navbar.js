2c22f11ccd79bfc226be0b26fb89f8825307940a
AldG99 <a.garciaglz17@gmail.com>
2024-12-16 12:33:36 -0600

    Añadir componente Navbar con funcionalidad de menú desplegable

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.scss';
 
 const Navbar = () => {
-  return <div className="">Navbar</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          WatchWorld
+        </Link>
+        <div
+          className={`menu-icon ${menuOpen ? 'open' : ''}`}
+          onClick={toggleMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={`nav-menu ${menuOpen ? 'active' : ''}`}>
+          <li className="nav-item">
+            <Link to="/" className="nav-link" onClick={toggleMenu}>
+              Inicio
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/productos" className="nav-link" onClick={toggleMenu}>
+              Relojes
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/carrito" className="nav-link" onClick={toggleMenu}>
+              Carrito
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
