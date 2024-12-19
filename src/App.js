4f7daca450c270fcf563477748fffbe5ff763788
AldG99 <a.garciaglz17@gmail.com>
2024-12-19 14:25:42 -0600

    Configurar rutas principales y estructura base de la aplicación con React Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './components/CartContext';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import ProductList from './components/ProductList';
@@ -7,23 +8,27 @@ import ProductDetail from './components/ProductDetail';
 import Cart from './components/Cart';
 import Checkout from './components/Checkout';
 import Footer from './components/Footer';
+import AdminLogin from './components/AdminLogin';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/productos" element={<ProductList />} />
-          <Route path="/producto/:id" element={<ProductDetail />} />
-          <Route path="/carrito" element={<Cart />} />
-          <Route path="/checkout" element={<Checkout />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="app-container">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/productos" element={<ProductList />} />
+            <Route path="/producto/:id" element={<ProductDetail />} />
+            <Route path="/carrito" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/admin" element={<AdminLogin />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
--- src/list/page.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ListPage = () => {
-  return <div className="">ListPage</div>;
-};
-
-export default ListPage;
